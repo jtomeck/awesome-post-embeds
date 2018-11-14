b22a8ae61d74f92ba9e88c990df95d9d4c285120
Jared Tomeck <jared.tomeck@kitware.com>
2018-11-14 11:56:31 -0500

    Plugin file created

---
+++ awesome-post-embeds.php
@@ -0,0 +1,13 @@
+<?php
+   /*
+   Plugin Name: Awesome Post Embeds
+   Plugin URI: https://github.com/jtomeck/awesome-post-embeds
+   description: >-
+  A plugin to easily embed posts from another Wordpress Blog using Wordpress's Rest API.
+   Version: 1.0
+   Author: Jared Tomeck
+   Author URI: https://github.com/jtomeck
+   License: GPL2
+   */
+?>
+
