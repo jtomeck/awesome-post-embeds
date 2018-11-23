862f3ae6bf6bf4620674530b59d893da63fdfc8c
Jared Tomeck <jared.tomeck@kitware.com>
2018-11-22 20:53:06 -0500

    Cleanup plugin file

--- awesome-post-embeds.php
+++ awesome-post-embeds.php
@@ -9,7 +9,9 @@
    License: GPL2
    */
 
-
+/*
+* INCLUDE PLUGIN STYLES
+*/
 function ape_enqueue_styles() {
     $plugin_url = plugin_dir_url( __FILE__ );
     wp_enqueue_style( 'style',  $plugin_url . "/style.css");
@@ -21,9 +23,7 @@ add_action( 'wp_enqueue_scripts', 'ape_enqueue_styles' );
 * Make API request for tags slug and returns the ID
 */
 function tag_id_from_slug( $api_url, $slug ) {
-  $tag = json_decode (
-    file_get_contents( $api_url . "/tags?slug=$slug" )
-  );
+  $tag = json_decode ( file_get_contents( $api_url . "/tags?slug=$slug" ) );
   return $tag[0]->id;
 }
 
@@ -32,9 +32,7 @@ function tag_id_from_slug( $api_url, $slug ) {
 * Make API request for categories slug and returns the ID
 */
 function category_id_from_slug( $api_url, $slug ) {
-  $category = json_decode (
-    file_get_contents( $api_url . "/categories?slug=$slug" )
-  );
+  $category = json_decode ( file_get_contents( $api_url . "/categories?slug=$slug" ) );
   return $category[0]->id;
 }
 
@@ -80,86 +78,72 @@ function ape_posts_func( $atts ) {
   // ob_start to allow returning of multiple lines of PHP/HTML
   ob_start();
 
-  // Set up query for http_build_query
-  $rest_query = array(
-    'per_page' => $atts['count']
-  );
-
-  // If user includes a tag, run function to make API request for tag
-  if( $atts['tag'] ) {
-    $rest_query['tags'] = tag_id_from_slug( $atts['url'] . "/wp-json/wp/v2", $atts['tag'] );
-  }
-
-  // If user includes a category, run function to make API request for category
-  if( $atts['category'] ) {
-    $rest_query['categories'] = category_id_from_slug( $atts['url'] . "/wp-json/wp/v2", $atts['category'] );
-  }
-
-  if( $atts['url'] ) { // Require a URL in shortcode
-
-    // Set variable for building wp-json URL
-    $query_url = $atts['url'] . "/wp-json/wp/v2/posts?" . http_build_query( $rest_query );
-
-    // Build wp-json URL
-    $blog_posts = json_decode(
-      file_get_contents( $query_url )
+    // Set up query for http_build_query
+    $rest_query = array(
+      'per_page' => $atts['count']
     );
 
-    /**
-    * POST STRUCTURE
-    *
-    * This is the structure of the returned posts with important variables injected in
-    */
+    // If user includes a tag, run function to make API request for tag
+    if( $atts['tag'] ) {
+      $rest_query['tags'] = tag_id_from_slug( $atts['url'] . "/wp-json/wp/v2", $atts['tag'] );
+    }
 
-    ?>
+    // If user includes a category, run function to make API request for category
+    if( $atts['category'] ) {
+      $rest_query['categories'] = category_id_from_slug( $atts['url'] . "/wp-json/wp/v2", $atts['category'] );
+    }
 
-    <div id="awesome_post_embed" class="ape_posts_container">
-      <div class="ape_posts_wrapper">
+    if( $atts['url'] ) { // Require a URL in shortcode
 
-        <?php foreach ( $blog_posts as $blog_post ) : ?>
+      // Set variable for building wp-json URL
+      $query_url = $atts['url'] . "/wp-json/wp/v2/posts?" . http_build_query( $rest_query );
 
-          <div class="ape_post">
+      // Build wp-json URL
+      $blog_posts = json_decode( file_get_contents( $query_url ) ); ?>
 
-            <?php if( $atts['image'] == true ) : ?>
-              <div class="ape_post_image_wrapper">
-                <?php if( ( $image_src = embed_url_from_post( $blog_post, $atts['image_size'] ) ) != null ) : ?>
-                  <a href="<?php echo $blog_post->link; ?>" <?php if( $atts['new_tab'] == true ) { echo 'target="_blank"'; } ?> >
-                    <img src="<?php echo $image_src; ?>" class="ape_post_image">
-                  </a>
-                <?php endif ?>
-              </div>
-            <?php endif; //if image ?>
+      <div id="awesome_post_embed" class="ape_posts_container">
+        <div class="ape_posts_wrapper">
 
-            <?php if( $atts['title'] == true || $atts['excerpt'] == true ) : ?>
-              <div class="ape_post_content">
+          <?php foreach ( $blog_posts as $blog_post ) : ?>
 
-                <?php if( $atts['title'] == true ) : ?>
-                  <h3 class="ape_post_title">
+            <div class="ape_post">
+
+              <?php if( $atts['image'] == true ) : ?>
+                <div class="ape_post_image_wrapper">
+                  <?php if( ( $image_src = embed_url_from_post( $blog_post, $atts['image_size'] ) ) != null ) : ?>
                     <a href="<?php echo $blog_post->link; ?>" <?php if( $atts['new_tab'] == true ) { echo 'target="_blank"'; } ?> >
-                      <?php echo $blog_post->title->rendered; ?>
+                      <img src="<?php echo $image_src; ?>" class="ape_post_image">
                     </a>
-                  </h3>
-                <?php endif; //if title ?>
+                  <?php endif ?>
+                </div>
+              <?php endif; //if image ?>
 
-                <?php if( $atts['excerpt'] == true ) : ?>
-                  <p><?php echo $blog_post->excerpt->rendered; ?></p>
-                <?php endif; //if excerpt ?>
-              </div>
+              <?php if( $atts['title'] == true || $atts['excerpt'] == true ) : ?>
+                <div class="ape_post_content">
 
-            <?php endif; //if excerpt or title ?>
-          </div>
+                  <?php if( $atts['title'] == true ) : ?>
+                    <h3 class="ape_post_title">
+                      <a href="<?php echo $blog_post->link; ?>" <?php if( $atts['new_tab'] == true ) { echo 'target="_blank"'; } ?> >
+                        <?php echo $blog_post->title->rendered; ?>
+                      </a>
+                    </h3>
+                  <?php endif; //if title ?>
 
-        <?php endforeach; ?>
+                  <?php if( $atts['excerpt'] == true ) : ?>
+                    <p><?php echo $blog_post->excerpt->rendered; ?></p>
+                  <?php endif; //if excerpt ?>
+                </div>
 
+              <?php endif; //if excerpt or title ?>
+            </div>
+
+          <?php endforeach; ?>
+
+        </div>
       </div>
-    </div>
-
-  <?php return ob_get_clean();
-        // end ob_start
-
-  }else{
-    return '<p>You need to specify a URL to the blog whose posts you would like to embed (using <strong>url="yourblogname.com"</strong> in your shortcode).</p>';
-  }
-
+    <?php }else{
+      return '<p>You need to specify a URL to the blog whose posts you would like to embed (using <strong>url="yourblogname.com"</strong> in your shortcode).</p>';
+    }
+  return ob_get_clean(); // end ob_start
 } //end ape_posts_func
 add_shortcode( 'ape_posts', 'ape_posts_func' );
